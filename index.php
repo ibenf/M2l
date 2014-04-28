66ddfd5d221bf6a8b7bd0b0dd1c6b8ff426e5423
ibenf <illyesbf@yahoo.fr>
2014-04-28 10:46:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include "_debut.inc.php";
+include "_controlesEtGestionErreurs.inc.php";
+
+echo'
+<div class="span8">
+Bienvenue sur le site de la Maison Des Ligues de Lorraine .  
+
+<p>Ce site a été effectué dans le cadre d un PPE (Projet Personel Encadré) <p>
+<p>Il permet de:</p>
+<p>Gérer les établissements (caractéristiques et capacités d accueil) acceptant d héberger les differentes ligues.</p>
+<li>consulter </li>
+<li>réaliser </li>
+<li>modifier les attributions des chambres aux groupes dans les établissements.</li>
+</div>
+
+
+
+
+</body>
+</html>
+';
+
+footer();
+
+?>
